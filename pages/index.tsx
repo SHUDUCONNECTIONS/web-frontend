f37b05f6d2bd905d9d6931989e56dcab51185f7c
ZwivhuyaV <135695758+ZwivhuyaV@users.noreply.github.com>
2023-11-03 00:22:20 +0200

    .

---
+++ pages/index.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Home = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    if (!username || !password) {
+      setError('Please enter both username and password');
+    } else if (password.length < 6) {
+      setError('Password should be at least 6 characters long');
+    } else {
+      // Perform login actions
+      console.log('Username:', username);
+      console.log('Password:', password);
+      setError('');
+    }
+  };
+
+  const handleSignUp = () => {
+    router.push('/signup'); // Replace '/signup' with the route of your sign-up page
+  };
+
+  return (
+    <div className="container">
+      <div className="form">
+        <h1 className="title">LOGIN</h1>
+        <div className="logo">
+          <img src="/carerunnerlogo.png" alt="Your Logo" className="logo-image" />
+        </div>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="input"
+        />
+        <br></br>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+        />
+        <br></br>
+        {error && <p className="error-message">{error}</p>}
+        <button onClick={handleLogin} className="loginButton">
+          Login
+        </button>
+        <br></br>
+        <button onClick={handleSignUp} className="signUpButton">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/signup.tsx
@@ -0,0 +1,117 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Home = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [physicalAddress, setPhysicalAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const handleSignUp = () => {
+    if (!firstName || !lastName || !contactNumber || !email || !physicalAddress || !password || !confirmPassword) {
+      setError('Please fill in all the fields');
+      return;
+    }
+
+    if (password.length < 8) {
+      setError('Password should be at least 8 characters long');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+
+    // Rest of the logic for handleSignUp function
+    router.push('/signup');
+  };
+
+  const handleLogin = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className="container">
+      <div className="form">
+        <h1 className="title">SIGN UP</h1>
+        <div className="logo">
+          <img src="/carerunnerlogo.png" alt="Your Logo" className="logo-image" />
+        </div>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          className="input"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          className="input"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Contact Number"
+          value={contactNumber}
+          onChange={(e) => setContactNumber(e.target.value)}
+          className="input"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="input"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Physical Address"
+          value={physicalAddress}
+          onChange={(e) => setPhysicalAddress(e.target.value)}
+          className="input"
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          className="input"
+        />
+        <br />
+        {error && <p className="error-message">{error}</p>}
+        <button onClick={handleSignUp} className="signUpButton">
+          Sign Up
+        </button>
+        <p className="already-account">
+          Already have an account?{' '}
+          <span className="login-link" onClick={handleLogin}>
+            Login
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
