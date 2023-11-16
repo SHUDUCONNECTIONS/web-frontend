e77b169c9d6a12ec2e256e6790ac730f195573fa
ZwivhuyaV <135695758+ZwivhuyaV@users.noreply.github.com>
2023-11-16 16:46:51 +0200

    menu

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,12 +1,10 @@
+// Import necessary libraries
 import React from 'react';
-import { useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/menu2.module.css'; // Import the CSS module
 
-
 const Home = () => {
   const router = useRouter();
-  const [error, setError] = useState<string>('');
 
   const handleBillingInfo = () => {
     router.push('/billing-page');
@@ -25,39 +23,51 @@ const Home = () => {
   };
 
   const handleAccountProfile = () => {
-    router.push('');
+    router.push('/account-profile'); // Redirect to the "account-profile" page
   };
 
   return (
     <div className={styles.container}>
       <a className={styles['card-menu']} onClick={() => console.log('Button 1 clicked')}>
         <img src="/menu1.png" alt="Car Icon" className={styles['card-image']} />
-        <button style={{ width: '100%' }} className={styles['card-button']}>Request Pickup</button>
+        <button style={{ width: '100%' }} className={styles['card-button']}>
+          Request Pickup
+        </button>
       </a>
 
       <a className={styles['card-menu']} onClick={handleUploadCase}>
         <img src="/menu2.png" alt="Car Icon" className={styles['card-image']} />
-        <button style={{ width: '100%' }} className={styles['card-button']}>Upload Case File</button>
+        <button style={{ width: '100%' }} className={styles['card-button']}>
+          Upload Case File
+        </button>
       </a>
 
       <a className={styles['card-menu']} onClick={handleFirmRecords}>
         <img src="/menu3.png" alt="Car Icon" className={styles['card-image']} />
-        <button style={{ width: '100%' }} className={styles['card-button']}>Law-Firm Records</button>
+        <button style={{ width: '100%' }} className={styles['card-button']}>
+          Law-Firm Records
+        </button>
       </a>
 
       <a className={styles['card-menu']} onClick={handleAccountProfile}>
         <img src="/menu4.png" alt="Car Icon" className={styles['card-image']} />
-        <button style={{ width: '100%' }} className={styles['card-button']}>Lawyer Account Profile</button>
+        <button style={{ width: '100%' }} className={styles['card-button']}>
+          Lawyer Account Profile
+        </button>
       </a>
 
       <a className={styles['card-menu']} onClick={handleMemberAccess}>
         <img src="/menu5.png" alt="Car Icon" className={styles['card-image']} />
-        <button style={{ width: '100%' }} className={styles['card-button']}>Membership Access</button>
+        <button style={{ width: '100%' }} className={styles['card-button']}>
+          Membership Access
+        </button>
       </a>
 
       <a className={styles['card-menu']} onClick={handleBillingInfo}>
         <img src="/menu6.png" alt="Car Icon" className={styles['card-image']} />
-        <button style={{ width: '100%' }} className={styles['card-button']}>Billing Information</button>
+        <button style={{ width: '100%' }} className={styles['card-button']}>
+          Billing Information
+        </button>
       </a>
     </div>
   );
