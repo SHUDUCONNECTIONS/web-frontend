ca916ffac35f7e7c22ea94af6c34720275e49359
Thulani Mathonsi <thulanianthony4@gmail.com>
2023-11-17 13:17:32 +0200

    thulani

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -44,11 +44,63 @@ const Home = () => {
 
   const handleAccountProfile = () => {
     router.push('');
-  };
+  }; 
 
   return (
     <div className={styles.container}>
 
+      <div className={`${styles.container2} ${styles.mobile}`}>
+        <div className={`${styles.form} ${styles.mobile}`}>
+          <div className={`${styles.logo} ${styles.mobile}`}>
+           <img src="/carerunnerlogo.png" alt="Your Logo" className={`${styles['logo-image']} ${styles.mobile}`} />
+          </div>
+
+          <button onClick={() => console.log('Button 1 clicked')} className={`${styles['button-menu']} ${styles.mobile}`}>
+            <span className="icon">
+              <FontAwesomeIcon icon={faCar} />
+            </span>
+            <span className={`${styles['button-text']} ${styles.mobile}`}>Pick Up Request</span>
+          </button>
+
+          <button onClick={handleUploadCase} className={styles['button-menu']}>
+            <span className="icon">
+             <FontAwesomeIcon icon={faFileUpload} />
+           </span>
+           <span className={styles['button-text']}>Upload Case</span>
+          </button>
+        
+          <button onClick={handleFirmRecords} className={styles['button-menu']}>
+            <span className="icon">
+              <FontAwesomeIcon icon={faFolder} />
+            </span>
+           <span className={styles['button-text']}>Firm Records</span>
+          </button>
+        
+           <button onClick={() => console.log('Button 4 clicked')} className={styles['button-menu']}>
+            <span className="icon">
+              <FontAwesomeIcon icon={faUser} />
+            </span>
+            <span className={styles['button-text']}>Account Profile</span>
+           </button>
+        
+           <button onClick={handleMemberAccess} className={styles['button-menu']}>
+             <span className="icon">
+              <FontAwesomeIcon icon={faUserShield} />
+            </span>
+            <span className={styles['button-text']}>Membership Access</span>
+           </button>
+        
+           <button onClick={handleBillingInfo} className={styles['button-menu']}>
+             <span className="icon">
+              <FontAwesomeIcon icon={faMoneyBill} />
+            </span>
+            <span className={styles['button-text']}>Billing Information</span>
+           </button>
+    
+        </div>
+      </div>
+
+      <div className={`${styles.container} ${styles.desktop}`}>
         <a href="/link-to-desired-url" className={styles['card-menu']} onClick={() => console.log('Button 1 clicked')}>
           <img src="/menu1.png" alt="Car Icon" className={styles['card-image']} />
           <button style={{ width: '100%' }} className={styles['card-button']}>Request Pickup</button>
@@ -74,13 +126,13 @@ const Home = () => {
           <button style={{ width: '100%' }} className={styles['card-button']}>Membership Access</button>
         </a>
 
-        <a href="/billing-page.tsx" className={styles['card-menu']} onClick={(handleBillingInfo) => console.log('Button 1 clicked')}>
+        <a href="/link-to-desired-url" className={styles['card-menu']} onClick={(handleBillingInfo) => console.log('Button 1 clicked')}>
           <img src="/menu6.png" alt="Car Icon" className={styles['card-image']} />
           <button style={{ width: '100%' }} className={styles['card-button']}>Billing Information</button>
         </a>
         
-        
       </div>
+    </div>
   );
 };
 
---
+++ pages/mobile.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './home.module.css'; // Import the CSS module
+import {
+  faCar,
+  faFileUpload,
+  faFolder,
+  faUser,
+  faUserShield,
+  faMoneyBill,
+} from '@fortawesome/free-solid-svg-icons';
+
+const Home = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string>('');
+
+  const handleLogin = () => {
+    console.log('Performing login actions');
+    setError('');
+  };
+
+  const handleSignUp = () => {
+    router.push('/signup');
+  };
+
+  const handleBillingInfo = () => {
+    router.push('/billing-page');
+  };
+
+  const handleMemberAccess = () => {
+    router.push('/membership-access');
+  };
+
+  const handleFirmRecords = () => {
+    router.push('/firm-records');
+  };
+
+  const handleUploadCase = () => {
+    router.push('/upload-case');
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <div className={styles.logo}>
+          <img src="/carerunnerlogo.png" alt="Your Logo" className={styles['logo-image']} />
+        </div>
+
+        <button onClick={() => console.log('Button 1 clicked')} className={styles['button-menu']}>
+          <span className="icon">
+            <FontAwesomeIcon icon={faCar} />
+          </span>
+          <span className={styles['button-text']}>Pick Up Request</span>
+        </button>
+        
+        <button onClick={handleUploadCase} className={styles['button-menu']}>
+          <span className="icon">
+            <FontAwesomeIcon icon={faFileUpload} />
+          </span>
+          <span className={styles['button-text']}>Upload Case</span>
+        </button>
+        
+        <button onClick={handleFirmRecords} className={styles['button-menu']}>
+          <span className="icon">
+            <FontAwesomeIcon icon={faFolder} />
+          </span>
+          <span className={styles['button-text']}>Firm Records</span>
+        </button>
+        
+        <button onClick={() => console.log('Button 4 clicked')} className={styles['button-menu']}>
+          <span className="icon">
+            <FontAwesomeIcon icon={faUser} />
+          </span>
+          <span className={styles['button-text']}>Account Profile</span>
+        </button>
+        
+        <button onClick={handleMemberAccess} className={styles['button-menu']}>
+          <span className="icon">
+            <FontAwesomeIcon icon={faUserShield} />
+          </span>
+          <span className={styles['button-text']}>Membership Access</span>
+        </button>
+        
+        <button onClick={handleBillingInfo} className={styles['button-menu']}>
+          <span className="icon">
+            <FontAwesomeIcon icon={faMoneyBill} />
+          </span>
+          <span className={styles['button-text']}>Billing Information</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
