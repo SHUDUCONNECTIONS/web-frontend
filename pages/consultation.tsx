519162ccc55b359103e58fb2af3c4601526158dd
ZwivhuyaV <135695758+ZwivhuyaV@users.noreply.github.com>
2023-12-01 15:41:31 +0200

    consultation

---
+++ pages/consultation.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styles from '../styles/consultation.module.css';
+
+const Consultation = () => {
+  const [selectedLawyer, setSelectedLawyer] = useState<string | null>(null);
+
+  const lawyerOptions = ['Criminal Law', 'Family Law', 'Business Law', 'Immigration Law'];
+
+  const handleLawyerSelection = (lawyerType: string) => {
+    setSelectedLawyer(lawyerType);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.lawyerSelection}>
+        <h1 className={styles.title}>Select a Type of Lawyer</h1>
+        <div className={styles.dropdownContainer}>
+          <select
+            className={styles.dropdown}
+            onChange={(e) => handleLawyerSelection(e.target.value)}
+          >
+            <option value="" disabled selected>Select a lawyer type</option>
+            {lawyerOptions.map((lawyer, index) => (
+              <option key={index} value={lawyer}>
+                {lawyer}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      {selectedLawyer && (
+        <div className={styles.chatroom}>
+          <h2 className={styles.chatTitle}>
+            Chatting with {selectedLawyer} Lawyer
+          </h2>
+          <div className={styles.chatContainer}>
+            {/* Chatroom component goes here */}
+            {/* You can use a third-party chat library or implement your own chat functionality */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Consultation;
